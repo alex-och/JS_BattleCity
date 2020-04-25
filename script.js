ddc6ce9df8db39392cc91370e357fd382c47c7c0
Alex <alex@gmail.com>
2020-04-25 12:25:47 +0300

    Collision detection added

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 var CANVAS = document.getElementById('field');
 var STEP = 5,
-    FPS = 60;
-var x, y,
+    FPS = 40;
+var A,
+    ENEMIES = [],
+    ENEMIES_AMOUNT = 6,
+    ELEMENTS = [],
     game,
     xv = 0,
     yv = 0,
@@ -10,6 +13,12 @@ var tmp_top,
     tmp_left,
     tmp_x,
     tmp_y;
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+
 
 function Bullet() {
     this.obj = document.createElement('div');
@@ -48,8 +57,8 @@ function Bullet() {
     }
     
     this.move = function() {
-        let tmp_x = this.obj.offsetTop + this.yv * STEP * 1.5;
-        let tmp_y = this.obj.offsetLeft + this.xv * STEP * 1.5;
+        let tmp_x = this.obj.offsetTop + this.yv * STEP * 2;
+        let tmp_y = this.obj.offsetLeft + this.xv * STEP * 2;
         
         if (tmp_x >= this.BORDER_TOP && tmp_x <= this.BORDER_BOTTOM) {
             this.obj.style.top = tmp_x + 'px';
@@ -68,12 +77,21 @@ function Bullet() {
 }
 
 // Tank object
-function Tank(left, top) {
+function Tank(left, top, tank_type) {
     this.obj = document.createElement('div');
     this.obj.style.top = top + 'px';
     this.obj.style.left = left + 'px';
     this.obj.style.transform = 'rotate(0deg)';
-    this.obj.className = 'x';
+    this.obj.className = tank_type; // tank_type = 'x' OR 'y'
+    
+    this.getSize = function(top, left) {
+        this.top = top;
+        this.left = left;
+        this.bottom = top + 50;
+        this.right = left + 50;
+    }
+    
+    this.getSize(top, left);
     // Adding a picture of a tank
     var temp = document.createElement('div');
     temp.className = 'x_inner';
@@ -81,9 +99,41 @@ function Tank(left, top) {
     var temp = document.createElement('div');
     temp.className = 'x_t';
     this.obj.appendChild(temp);
-
+    
     this.bullet = new Bullet();
     this.shoot_act = false;
+    
+    this.CollisionDetector = function(xv, yv) {
+        if (xv != 0) {
+            this.coord_const = this.obj.offsetLeft + 50 * xv;
+            this.line_max = this.obj.offsetTop + 50;
+            this.line_min = this.obj.offsetTop - 50;
+        }
+        if (yv != 0) {
+            this.coord_const = this.obj.offsetTop + 50 * yv;
+            this.line_max = this.obj.offsetLeft + 50;
+            this.line_min = this.obj.offsetLeft - 50;
+        }
+        for(let i = 0; i < ELEMENTS.length; i++) {
+            if (ELEMENTS[i] !== this) {
+                if (xv != 0) {
+                    if (ELEMENTS[i].obj.offsetLeft == this.coord_const && +
+                        ELEMENTS[i].obj.offsetTop < this.line_max && +
+                        ELEMENTS[i].obj.offsetTop > this.line_min) {
+                        return false;
+                    }
+                }
+                if (yv != 0) {
+                    if (ELEMENTS[i].obj.offsetTop == this.coord_const && +
+                        ELEMENTS[i].obj.offsetLeft < this.line_max && +
+                        ELEMENTS[i].obj.offsetLeft > this.line_min) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
     
     this.BORDER_TOP = 0;
     // this.BORDER_BOTTOM = CANVAS.offsetHeight - this.obj.offsetHeight;
@@ -92,26 +142,36 @@ function Tank(left, top) {
     // this.BORDER_RIGHT = CANVAS.offsetWidth - this.obj.offsetWidth;
     this.BORDER_RIGHT = CANVAS.offsetWidth - 50;
 
-    this.move = function(xv, yv) {
+    this.xv = 0;
+    this.yv = 0;
+
+    this.move = function(xv=this.xv, yv=this.yv) {
         // actual moving
-        let tmp_top = this.obj.offsetTop + yv * STEP;
-        let tmp_left = this.obj.offsetLeft + xv * STEP;
-        
-        if (tmp_top >= this.BORDER_TOP && tmp_top <= this.BORDER_BOTTOM) {
-            this.obj.style.top = tmp_top + 'px';
-        }
-        if (tmp_left >= this.BORDER_LEFT && tmp_left <= this.BORDER_RIGHT) {
-            this.obj.style.left = tmp_left + 'px';
-        }
-        // turning the tank
-        if (xv == -1) {
-            this.obj.style.transform = 'rotate(0deg)';
-        } else if (xv == 1) {
-            this.obj.style.transform = 'rotate(180deg)';
-        } else if (yv == -1) {
-            this.obj.style.transform = 'rotate(90deg)';
-        } else if (yv == 1) {
-            this.obj.style.transform = 'rotate(270deg)';
+        if (xv != 0 || yv != 0) {
+            let tmp_top = this.obj.offsetTop + yv * STEP;
+            let tmp_left = this.obj.offsetLeft + xv * STEP;
+
+            if (this.CollisionDetector(xv, yv)) {
+                if (tmp_top >= this.BORDER_TOP && tmp_top <= this.BORDER_BOTTOM) {
+                    this.obj.style.top = tmp_top + 'px';
+                    this.getSize(tmp_top, tmp_left);
+                }
+                if (tmp_left >= this.BORDER_LEFT && tmp_left <= this.BORDER_RIGHT) {
+                    this.obj.style.left = tmp_left + 'px';
+                    this.getSize(tmp_top, tmp_left);
+                }
+            }
+            
+            // turning the tank
+            if (xv == -1) {
+                this.obj.style.transform = 'rotate(0deg)';
+            } else if (xv == 1) {
+                this.obj.style.transform = 'rotate(180deg)';
+            } else if (yv == -1) {
+                this.obj.style.transform = 'rotate(90deg)';
+            } else if (yv == 1) {
+                this.obj.style.transform = 'rotate(270deg)';
+            }
         }
         // shooting
         if (this.shoot_act) {
@@ -132,17 +192,51 @@ function Tank(left, top) {
 }
 
 // Main cycle
+
+function Bot() {
+    let xv, yv;
+    for(let i = 0; i < ENEMIES.length; i++) {
+        xv = 0;
+        yv = 0;
+        xv = getRndInteger(-1, 1);
+        if (xv == 0) {
+            yv = getRndInteger(-1, 1);
+        }
+        ENEMIES[i].xv = xv;
+        ENEMIES[i].yv = yv;
+        if (getRndInteger(0, 1)) {
+            ENEMIES[i].shoot();
+        }
+    }
+}
+
 function Runtime() {
-    x.move(xv, yv);
+    for(let i = 0; i < ELEMENTS.length; i++) {
+        if (i == ELEMENTS.length - 1) {
+            ELEMENTS[i].move(xv, yv);
+        } else {
+            ELEMENTS[i].move();
+        }
+    }
 }
 
 // Event listeners
 window.addEventListener("load", function() {
-    x = new Tank(100, 100);
-    x.add();
-    game = setInterval(Runtime, 1000 / FPS);
-})
+    A = new Tank(100, 100, 'x');
+    A.add();
 
+    for(let i = 0; i < ENEMIES_AMOUNT; i++) {
+        ENEMIES.push(new Tank(100*(i+1), 0, 'y'));
+        ENEMIES[i].add();
+        ELEMENTS.push(ENEMIES[i]);
+    }
+
+    ELEMENTS.push(A);
+
+    game = setInterval(Runtime, 1000 / FPS);
+    setInterval(Bot, 1000);
+})
+// Controller
 window.addEventListener("keydown", function() {
     if (this.event.code == 'ArrowUp') {
         [xv, yv] = [0, -1];
@@ -158,10 +252,10 @@ window.addEventListener("keydown", function() {
         key_active = 'ArrowRight';
     }
 })
-
+// Shooting
 window.addEventListener("keypress", function() {
     if (this.event.code == 'KeyX') {
-        x.shoot();
+        A.shoot();
     }
 })
 
@@ -170,3 +264,4 @@ window.addEventListener("keyup", function() {
         [xv, yv] = [0, 0];
     }
 })
+
